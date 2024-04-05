edb3652bf909e4d7eb4586aa1f7140fe4932bdee
Alexandra <alexandervirtuous14@gmail.com>
2024-04-05 21:51:47 +0100

    modified:   Routes/adminDeposit.js

--- Routes/adminDeposit.js
+++ Routes/adminDeposit.js
@@ -23,7 +23,9 @@ router.post("/", async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    return res.status(200).json({ message: "Deposit successful" });
+    return res
+      .status(200)
+      .json({ message: "Deposit successful", status: "ok" });
   } catch (err) {
     console.error("Error depositing amount:", err);
     return res.status(500).json({ error: "Internal server error" });
