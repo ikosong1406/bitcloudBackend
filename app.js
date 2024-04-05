f55034d6a81697b2c018858ceabfc6fab6986092
Alexandra <alexandervirtuous14@gmail.com>
2024-04-05 20:10:31 +0100

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 const allUsers = require("./Routes/allUsers");
 const userdata = require("./Routes/userdata");
 const register = require("./Routes/register");
