5d90ed6bc64daed1897296e987ddc918657f4566
ikosong14 <alexandervirtuous14@gmail.com>
2024-08-13 17:41:55 +0100

    modified:   utils/mail.js

--- utils/mail.js
+++ utils/mail.js
@@ -1,30 +1,5 @@
 const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   host: "mail.trustleger.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: "support@trustleger.com",
-//     pass: "Trustleger14",
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// async function sendMail(to, subject, text, html) {
-//   const mailOptions = {
-//     from: "support@trustleger.com",
-//     to,
-//     subject,
-//     text,
-//     html,
-//   };
-
-//   return transporter.sendMail(mailOptions);
-// }
-
 const transporter = nodemailer.createTransport({
   host: "mail.bitvelar.com",
   port: 465, //465 is true & other port are false
