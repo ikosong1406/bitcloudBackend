1d7d95e061d1920407869aca59257ffae58e28d2
ikosong14 <alexandervirtuous14@gmail.com>
2024-10-26 16:14:13 +0100

    modified:   Schemas/TransactionDetails.js

--- Schemas/TransactionDetails.js
+++ Schemas/TransactionDetails.js
@@ -23,7 +23,7 @@ const transactionSchema = new mongoose.Schema({
   },
   method: {
     type: String,
-    enum: ["tether", "bitcoin", "paypal"],
+    enum: ["usdt", "bitcoin"],
   },
   status: {
     type: String,
